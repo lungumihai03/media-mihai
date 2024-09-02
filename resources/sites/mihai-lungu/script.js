6d0c858900d41a5397a6017cbdf6427a488c0b42
Mihai Lungu <lungu.mihai03@gmail.com>
2024-09-02 21:45:24 +0300

    Update script.js

--- resources/sites/mihai-lungu/script.js
+++ resources/sites/mihai-lungu/script.js
@@ -1,13 +1,20 @@
- document.addEventListener("DOMContentLoaded", function() {
-      var currentYear = new Date().getFullYear();
-      var footerYear = document.getElementById("footer-year");
-      footerYear.textContent = currentYear;
-    });
-   // Verificăm ora pentru a decide modul de afișare (luminos sau întunecat)
-      // script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var currentYear = new Date().getFullYear();
+    var footerYear = document.getElementById("footer-year");
+    footerYear.textContent = currentYear;
 
-const currentHour = new Date().getHours();
-const body = document.body;
+    // Funcție care verifică ora și setează modul dark/light
+    function checkTimeForDarkMode() {
+        const currentHour = new Date().getHours();
+        const body = document.body;
 
-body.classList.toggle("dark-mode", currentHour >= 21 || currentHour < 7);
-body.classList.toggle("light-mode", !(currentHour >= 21 || currentHour < 7));
+        body.classList.toggle("dark-mode", currentHour >= 20 || currentHour < 7);
+        body.classList.toggle("light-mode", !(currentHour >= 20 || currentHour < 7));
+    }
+
+    // Verificăm imediat când pagina se încarcă
+    checkTimeForDarkMode();
+
+    // Setăm un interval de 30 minute pentru a verifica din nou ora
+    setInterval(checkTimeForDarkMode, 30 * 60 * 1000); // 30 minute în milisecunde
+});
