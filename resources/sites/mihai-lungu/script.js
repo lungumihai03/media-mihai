932371b36892a076b6721dfc4b19ca356f5996d1
Mihai Lungu <lungu.mihai03@gmail.com>
2025-01-27 01:39:23 +0200

    Update script.js

--- resources/sites/mihai-lungu/script.js
+++ resources/sites/mihai-lungu/script.js
@@ -1,20 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var currentYear = new Date().getFullYear();
-    var footerYear = document.getElementById("footer-year");
-    footerYear.textContent = currentYear;
+ document.addEventListener("DOMContentLoaded", function() {
+      var currentYear = new Date().getFullYear();
+      var footerYear = document.getElementById("footer-year");
+      footerYear.textContent = currentYear;
 
-    // Funcție care verifică ora și setează modul dark/light
-    function checkTimeForDarkMode() {
+      // Funcție care verifică ora și setează modul dark/light
+      function checkTimeForDarkMode() {
         const currentHour = new Date().getHours();
         const body = document.body;
 
         body.classList.toggle("dark-mode", currentHour >= 18 || currentHour < 7);
         body.classList.toggle("light-mode", !(currentHour >= 18 || currentHour < 7));
-    }
 
-    // Verificăm imediat când pagina se încarcă
-    checkTimeForDarkMode();
+        // Setăm imaginea în funcție de modul dark/light
+        if (body.classList.contains('dark-mode')) {
+          document.getElementById('cetate').innerHTML = '<img src="../dark-cetate.png" alt="Source Wikipedia.org" title="Source Wikipedia.org" class="cetate">';
+        } else {
+          document.getElementById('cetate').innerHTML = '<img src="../cetate.png" alt="Source Wikipedia.org" title="Source Wikipedia.org" class="cetate">';
+        }
+      }
 
-    // Setăm un interval de 30 minute pentru a verifica din nou ora
-    setInterval(checkTimeForDarkMode, 30 * 60 * 1000); // 30 minute în milisecunde
-});
+      // Verificăm imediat când pagina se încarcă
+      checkTimeForDarkMode();
+
+      // Setăm un interval de 30 minute pentru a verifica din nou ora
+      setInterval(checkTimeForDarkMode, 30 * 60 * 1000); // 30 minute în milisecunde
+    });
