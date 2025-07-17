55328123e86965c3489d2e2d6e3913a339ca59ca
Mihai Lungu <lungu.mihai03@gmail.com>
2025-07-17 12:09:10 +0300

    Create mscript.h

---
+++ mscript.h
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<fstream>
+#include<iterator>
+#include<functional>
+#include<limits>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#include<random>
+#define us using namespace 
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(v) ((int)(v).size())
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define forr(i, a, b) for(int i = a; i <= b; i++)
+#define ford(i, a, b) for(int i = a; i >= b; i--)
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define in(x) cin >> x
+#define out(x) cout << x
+#define outln(x) cout << x << '\n'
+#define outsp(x) cout << x << ' '
+#define endl '\n'
+#define dbg(x) cerr << #x << " = " << x << endl
+#define in2(x, y) cin >> x >> y
+#define in3(x, y, z) cin >> x >> y >> z
+#define out2(x, y) cout << x << ' ' << y << '\n'
+#define out3(x, y, z) cout << x << ' ' << y << ' ' << z << '\n'
+#define vec(type, name) vector<type> name
+#define vec2d(type, name, n, m) vector<vector<type>> name(n, vector<type>(m))
+#define read_vec(v, n) forn(i, n) cin >> v[i]
+#define print_vec(v) forn(i, sz(v)) cout << v[i] << ' '; cout << '\n'
+#define read_mat(mat, n, m) forn(i, n) forn(j, m) cin >> mat[i][j]
+#define print_mat(mat, n, m) forn(i, n) { forn(j, m) cout << mat[i][j] << ' '; cout << '\n'; }
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << '\n'
