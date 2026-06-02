d8225fae0a61a3d49e966a40574a00255a967417
Mihai Lungu <lungu.mihai03@gmail.com>
2026-06-02 17:42:08 +0300

    Create awards.js

---
+++ resources/sites/mihai-lungu/awards.js
@@ -0,0 +1,41 @@
+async function loadAwards(lang) {
+
+    const response = await fetch('/resources/xml/awards.xml');
+    const xmlText = await response.text();
+
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(xmlText, "text/xml");
+
+    const awards = xml.getElementsByTagName("award");
+
+    let html = "";
+
+    for (let i = 0; i < awards.length; i++) {
+
+        const award = awards[i];
+
+        const year =
+            award.getElementsByTagName("year")[0].textContent;
+
+        const link =
+            award.getElementsByTagName("link")[0].textContent;
+
+        const title =
+            award.getElementsByTagName("title_" + lang)[0].textContent;
+
+        const description =
+            award.getElementsByTagName("description_" + lang)[0].textContent;
+
+        html += `
+            <p>
+                ${year} -
+                <a href="${link}" target="_blank" class="premii">
+                    <u>${title}</u>
+                </a>
+                ${description}
+            </p>
+        `;
+    }
+
+    document.getElementById("awards-container").innerHTML = html;
+}
